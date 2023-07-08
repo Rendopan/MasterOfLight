ed5085739af6336c0aab2d2c9f1137cdc4645c2d
Rendopan_PC\Rendopan <rendopan@gmail.com>
2023-07-08 09:40:25 +0200

    First Commit Master of Light
    First Commit.
    Player controller movement:
    Andrea move in a map using nav mesh controller.
    Animation controller taken from unity asset akane.

---
+++ MasterOfLight/Assets/Scripts/AndreaController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AndreaController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent character;
+    public Animator characterAnimator;
+    public GameObject targetDest;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitPoint;
+
+            if(Physics.Raycast(ray, out hitPoint)) 
+            {
+                targetDest.transform.position = hitPoint.point;
+                character.SetDestination(hitPoint.point);
+            }
+        }
+
+        if(character.velocity != Vector3.zero)
+        {
+            characterAnimator.SetBool("isWalking", true);
+        }
+        else if (character.velocity == Vector3.zero) 
+        {
+            characterAnimator.SetBool("isWalking", false);
+        }
+    }
+}
