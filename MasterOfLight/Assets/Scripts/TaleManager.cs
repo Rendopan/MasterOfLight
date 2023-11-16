05030efa7afa0e2a2c424a7b22ac25c96be950e4
Rendopan_PC\Rendopan <rendopan@gmail.com>
2023-11-16 20:30:15 +0100

    14. POI and Navigation
    - POI update on GDD 3.0.
    - Put a cube in each POI of the Map.
    - Update Andrea position and first navigation on map tried. Update Corinaldo houses position or presence.

--- MasterOfLight/Assets/Scripts/TaleManager.cs
+++ MasterOfLight/Assets/Scripts/TaleManager.cs
@@ -23,7 +23,10 @@ public class TaleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        { 
+            Application.Quit();
+        }
     }
     public int CurrentStroke
     { 
